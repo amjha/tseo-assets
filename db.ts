e996dd20729bf9523b1b12862367c3861a796739
Amit Jha <amitjha@usc.edu>
2020-06-10 17:16:50 -0400

    DB env variables

--- db.ts
+++ db.ts
@@ -8,11 +8,11 @@ export async function connectionHandle() {
         try {
             // @ts-ignore
             conn = await createConnection({
-                type: 'mssql',
+                type: process.env.DB_TYPE,
                 host: process.env.DB_SERVER,
                 port: process.env.DB_PORT,
                 username: process.env.DB_USER,
-                password: process.env.DB_PASS,
+                password: process.env.DB_PASSWORD,
                 database: process.env.DB_NAME,
                 entities: [
                     Photo

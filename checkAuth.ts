551748fcab7cad071550be33f2213f6b1714ca25
Amit Jha <amitjha@usc.edu>
2020-06-10 17:32:34 -0400

    Formatting

--- checkAuth.ts
+++ checkAuth.ts
@@ -3,11 +3,8 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     console.log('Authorizing...');
     try {
-        // console.log(req.headers);
-        console.log(req.headers.Authorization);
-        console.log(req.headers.authorization);
         const token = req.headers.authorization.split(' ')[1];
-        jwt.verify(token, 'cThIIoDvwdueQB468K5xDc5633seEFoqwxjF_xSJyQQ');
+        jwt.verify(token, process.env.JWT_SECRET);
         next();
     } catch (err) {
         console.log('Authorization failed');

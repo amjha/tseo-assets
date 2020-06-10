51c74b20d4a1cbb723099b161065a831d0ba2d13
Amit Jha <amitjha@usc.edu>
2020-06-10 17:33:09 -0400

    console removal

--- checkAuth.ts
+++ checkAuth.ts
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-    console.log('Authorizing...');
     try {
         const token = req.headers.authorization.split(' ')[1];
         jwt.verify(token, process.env.JWT_SECRET);

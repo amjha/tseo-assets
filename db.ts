aa0cadb998cd5c7abbf665d98d0c1e2928528b82
Amit Jha <amitjha@usc.edu>
2020-06-10 16:36:33 -0400

    Dead code removal

--- db.ts
+++ db.ts
@@ -1,40 +1,8 @@
-/*
-require('dotenv').config();
-const sql = require('mssql');
-console.log(process.env.DB_USER)
-var sqlConfig = {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    server: process.env.DB_SERVER,
-    database: process.env.DB_NAME,
-};
-
-async function executeQuery(){
-    try{
-        let pool = await sql.connect(sqlConfig);
-        let res = pool.request().execute("test_sys_pro") //.query("select top 10 * from GURU_DEFINITIONS;");
-        res.then(r=>{
-            r.recordset.forEach(v=>{console.dir(v)})
-
-        })
-    } catch(err){
-        console.log(err)
-    }
-}
-
-sql.on("error", err=>{
-    console.log(err)
-})
-
-executeQuery();
-
-*/
-// -----------------------------------------------------------------------------------------------------------------------
-
-
 import {createConnection} from 'typeorm';
 import {Photo} from './model';
+
 let conn;
+
 export async function connectionHandle() {
     if (conn === undefined) {
         try {
@@ -59,5 +27,3 @@ export async function connectionHandle() {
     }
     return conn;
 }
-
-
